8839045ded2e891d3f32df5897cd8107c0877981
Zwanzen <Cleantob@hotmail.com>
2025-06-30 07:01:50 +0200

    Testing

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -172,26 +172,28 @@ public class CameraManager : MonoBehaviour
     private float BounceEaseOut(float t)
     {
         float bounce = bounceAmplitude;
+        return bounce * (7.5625f * t * t);
+        /*
+    if (t < 1 / 2.75f)
+    {
+    }
 
-        if (t < 1 / 2.75f)
-        {
-            return bounce * (7.5625f * t * t);
-        }
-        else if (t < 2 / 2.75f)
-        {
-            t -= 1.5f / 2.75f;
-            return bounce * (7.5625f * t * t + 0.75f);
-        }
-        else if (t < 2.5 / 2.75)
-        {
-            t -= 2.25f / 2.75f;
-            return bounce * (7.5625f * t * t + 0.9375f);
-        }
-        else
-        {
-            t -= 2.625f / 2.75f;
-            return bounce * (7.5625f * t * t + 0.984375f);
-        }
+    else if (t < 2 / 2.75f)
+    {
+        t -= 1.5f / 2.75f;
+        return bounce * (7.5625f * t * t + 0.75f);
+    }
+    else if (t < 2.5 / 2.75)
+    {
+        t -= 2.25f / 2.75f;
+        return bounce * (7.5625f * t * t + 0.9375f);
+    }
+    else
+    {
+        t -= 2.625f / 2.75f;
+        return bounce * (7.5625f * t * t + 0.984375f);
+    }
+    */
     }
 
     private void SetTargetGroup(Transform _target, Transform _moon)
